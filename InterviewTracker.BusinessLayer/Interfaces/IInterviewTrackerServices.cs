3ce3f9b6fb13c2cc7d77adaa0346ba3716504131
Uma Kumar <31764895+umakumarsingh@users.noreply.github.com>
2021-01-18 11:09:12 +0530

    Update IInterviewTrackerServices.cs

--- InterviewTracker.BusinessLayer/Interfaces/IInterviewTrackerServices.cs
+++ InterviewTracker.BusinessLayer/Interfaces/IInterviewTrackerServices.cs
@@ -1,7 +1,5 @@
 ﻿using InterviewTracker.Entities;
-using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace InterviewTracker.BusinessLayer.Interfaces
